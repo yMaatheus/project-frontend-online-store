7f8fe32280eb524bb5f84a112cdeeb2d9e8ab21d
Anderson Rodrigues da Silva <omicron_rpl@hotmail.com>
2022-03-12 15:40:22 -0300

    Fixed lint errors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import CategoryList from './CategoryList';
 import { Link } from 'react-router-dom';
+import CategoryList from './CategoryList';
 
 // function App() {
 class Home extends React.Component {
@@ -8,11 +8,11 @@ class Home extends React.Component {
     return (
       <div className="main">
         <CategoryList />
-          <header className="App-header">
-            <input />
-            <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
-            <ul>
-              <span data-testid="home-initial-message">
+        <header className="App-header">
+          <input />
+          <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
+          <ul>
+            <span data-testid="home-initial-message">
               Digite algum termo de pesquisa ou escolha uma categoria.
             </span>
           </ul>
