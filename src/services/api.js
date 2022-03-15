8231c7962f2a76b284faafe5e4bb262dce639068
Matheus <stvgamer74@gmail.com>
2022-03-15 16:29:18 -0300

    Corrigindo erro de lint, vamos ser felizes :D

--- src/services/api.js
+++ src/services/api.js
@@ -19,11 +19,8 @@ export async function getProductByProductId(productId) {
 export function addToCart(productName, quantity = 1) {
   if (localStorage.length > 0) {
     const previousCart = JSON.parse(localStorage.getItem('cart'));
-    localStorage.setItem(
-      'cart', JSON.stringify(
-        [...previousCart, { name: productName, quantity }],
-      ),
-    );
+    localStorage.setItem('cart',
+      JSON.stringify([...previousCart, { name: productName, quantity }]));
     return;
   }
   localStorage.setItem('cart', JSON.stringify([{ name: productName, quantity }]));
