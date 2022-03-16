b8a3d4c44c4a119d76369005b1a4d8669cb37804
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2022-03-16 17:10:37 -0300

    Adiciona form de finalizaçao de compra no componente Checkout

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -3,7 +3,50 @@ import React from 'react';
 class Checkout extends React.Component {
   render() {
     return (
-      <h1>Checkout</h1>
+      <div>
+        <h1>Checkout</h1>
+        <form>
+          <fieldset>
+            <input
+              type="text"
+              name="fullname"
+              data-testid="checkout-fullname"
+              placeholder="Nome Completo"
+            />
+            <input
+              type="text"
+              name="email"
+              data-testid="checkout-email"
+              placeholder="e-mail"
+            />
+            <input
+              type="text"
+              name="cpf"
+              data-testid="checkout-cpf"
+              placeholder="CPF"
+            />
+            <input
+              type="text"
+              name="phone"
+              data-testid="checkout-phone"
+              placeholder="Telefone"
+            />
+            <input
+              type="text"
+              name="cep"
+              data-testid="checkout-cep"
+              placeholder="CEP"
+            />
+            <input
+              type="text"
+              name="address"
+              data-testid="checkout-address"
+              placeholder="Endereço"
+            />
+            <button type="button">Finalizar</button>
+          </fieldset>
+        </form>
+      </div>
     );
   }
 }
